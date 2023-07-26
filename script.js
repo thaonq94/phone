2ad839074f0ac7668cdfb70525c6fd6801d3a554
Ngô Quang Thảo <quangthao.k12.cse@gmail.com>
2023-07-26 15:24:01 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+$('#phone-btn').click(function () {
+  var phone = $('#input__num').val();
+  window.open('etelecom://vn.etelecom.appcall.cs/outbound?phone=' + phone);
+});
+
+$(document).ready(function () {
+  $('.dial-pad .numbers').click(function () {
+    var actualNum = $('#input__num').val();
+    var numbers = $(this).attr('data-number');
+    var number = actualNum + numbers;
+    $('#input__num').val(number);
+    $('#delet').addClass('visible');
+    $('.css-mobile .dail-screen').addClass('bordered');
+  });
+
+  $('#delet').click(function () {
+    var inputString = $('#input__num').val();
+    var shortenedString = inputString.substr(0, inputString.length - 1);
+    $('#input__num').val(shortenedString);
+    if (inputString.length == 1) {
+      $(this).removeClass('visible');
+      $('.css-mobile .dail-screen').removeClass('bordered');
+    }
+  });
+
+  var DateTime = new Date();
+  var strHours = DateTime.getHours();
+  var strMinutes = DateTime.getMinutes();
+  var setTime = strHours + ':' + strMinutes;
+  $('.css-mobile .left-time').html(setTime);
+
+  $(function () {
+    $('#drage-btn').sortable({
+      revert: true,
+    });
+    $('#drage-btn').draggable({
+      connectToSortable: '#drage-btn',
+      revert: 'invalid',
+    });
+    $('.home-btn-wrapper').disableSelection();
+  });
+});
